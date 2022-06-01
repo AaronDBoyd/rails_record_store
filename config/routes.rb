58e056a1e1ff7bd03021aac8165c3035478873e0
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-01 03:04:07 -0700

    add artist routes, views, and link to albums

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'albums#index'
+  root to: 'artists#index'
   resources :albums do 
     resources :songs
   end
