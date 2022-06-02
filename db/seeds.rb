8428ca85db2b441469feed0b1a0e9043253ba20e
Aaron Boyd <a.boyd91@yahoo.com>
2022-06-01 19:02:35 -0700

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# album_list = [
-#   ["Thriller", "Pop"],
-#   ["back in black", "Rock"],
-#   ["The Bodyguard", "Soul"],
-#   ["Bat out of hell", "Rock"]
-# ]
-
-# album_list.each do |name, genre|
-#   Album.create( name: name, genre: genre )
-# end
-
-# Spice.destroy_all
-
-# 50.times do |index|
-#   Spice.create!(name: Faker::Food.spice,
-#                         description: Faker::Lorem.sentence(20, false, 0).chop,
-#                         image_url: "site.com/here_is_a_picture_of_cardamom.jpg")
-# end
-
-# p "Created #{Spice.count} spices"
 
 Album.destroy_all
 album_id_array = []
@@ -37,15 +9,11 @@ end
 p "Created #{Album.count} albums"
 
 p album_id_array
-# Song.destroy_all
-# num = 1
-# # num is the id number of the first album in the table. 
-# 10.times do |index|
+
 album_id_array.each do |num|
   7.times do |index|
   Song.create!(name: Faker::Superhero.name,lyrics: Faker::Lorem.paragraph_by_chars(number: 50), album_id: num)   
   end
-#   num += 1
 end
 
 p "Created #{Song.count} songs"
